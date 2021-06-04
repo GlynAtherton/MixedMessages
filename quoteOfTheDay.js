bd74a66c6af856790aefc5ff58a33bc771a76c8d
GlynAtherton <gatherto@yahoo.co.uk>
2021-06-04 09:42:01 +0100

    first commit

---
+++ quoteOfTheDay.js
@@ -0,0 +1,21 @@
+// Quotes from "The Meaning of Liff"
+let quotes = [
+    "Ardcrony (n.) : A remote acquaintance passed off as 'a very good friend of mine' by someone trying to impress people.",
+
+    "Symond’s Yat (n.) : The little spoonful inside the lid of a recently opened boiled egg.",
+
+    "Trantlemore (vb.) : To make a noise like a train crossing a set of points.",
+
+    "Beccles (n.) : The small bone buttons placed in bacon sandwiches by unemployed guerrilla dentist.",
+
+    "Scraptoft (n.) : The absurd flap of hair a vain and balding man grows long above one ear to comb it to the other ear.",
+
+    "Clunes (pl.n.) : People who just won't go.",
+
+    "Skenfrith (n.) : The flakes of athlete's foot found inside socks.",
+
+    "Huttoft (n.) : The fibrous algae which grows in the dark, moist environment of trouser turn-ups.",
+];
+
+let quote = Math.floor(Math.random() * quotes.length);
+console.log(quotes[quote]);
